9e667f1dc0df995ef254fd50753ed39862491e03
luisdev97 <amadormontorodavid@gmail.com>
2020-04-24 14:35:34 +0200

    refact(Query): refactor anomalies queries using fragments

--- src/modules/Anomalies/graphql/queries/anomalies.ts
+++ src/modules/Anomalies/graphql/queries/anomalies.ts
@@ -1,15 +1,36 @@
 import gql from "graphql-tag";
 
+
+const REQUIRED_FIELDS = gql`
+    fragment requiredFields on Anomaly {
+        id
+        creator {
+            firstname 
+            lastname
+        }
+        type
+        description
+    }
+`
+
 export const GET_ANOMALIES_QUERY = gql`
     query anomalies {
         getAnomalies {
-            id
-            creator {
-                firstname 
-                lastname
-            }
-            type
-            description
+            ...requiredFields
         }
-    }
+    }${REQUIRED_FIELDS}
+`
+
+export const GET_ONE_ANOMALY_QUERY = gql`
+    query anomaly($id: Int) {
+        getAnomaly(id: $id ){
+            ...requiredFields
+            incidents {
+                title
+                maxResearchers
+                resolved
+                location
+            }
+        }
+    }${REQUIRED_FIELDS}
 `
