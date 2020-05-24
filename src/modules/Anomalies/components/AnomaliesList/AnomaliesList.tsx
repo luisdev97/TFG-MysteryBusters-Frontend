772c9b3642068101eb9897addd07378d3003d766
luisdev97 <amadormontorodavid@gmail.com>
2020-05-24 19:37:14 +0200

    feat: Anomalies pagination

--- src/modules/Anomalies/components/AnomaliesList/AnomaliesList.tsx
+++ src/modules/Anomalies/components/AnomaliesList/AnomaliesList.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { AnomaliesListProps } from "../../props/Anomalies";
-import { List, Popup, Button } from "semantic-ui-react";
+import { List, Popup, Button, Pagination } from "semantic-ui-react";
 import AnomalyItem from "./AnomalyItem";
 import "./AnomaliesList.css";
 import CreateAnomalyForm from "../Form/AnomalyForm";
+import usePagination from "../../../../hooks/usePagination";
+import { Anomaly } from "../../models/entities/Anomaly";
 
 const style = {
   borderRadius: 0,
@@ -11,18 +13,29 @@ const style = {
   padding: "2em"
 };
 
-function AnomaliesList({ anomalies, create, update, remove }: AnomaliesListProps) {
+function AnomaliesList({
+  anomalies,
+  create,
+  update,
+  remove
+}: AnomaliesListProps) {
   const [visibleForm, setVisibleForm] = useState<boolean>(false);
+  const pagination = usePagination<Anomaly>(anomalies, 2);
 
   return (
     <List className="w-50 bg-danger mx-auto mt-5 pt-5 pb-5">
       <Popup
         trigger={
-          <Button className="anomalyPopup-trigger" color="vk">
+          <Button className="anomalyPopup-trigger d-block" color="vk">
             Crear anomalía
           </Button>
         }
-        content={<CreateAnomalyForm mutation={create} closeForm={ () => setVisibleForm(false) }/>}
+        content={
+          <CreateAnomalyForm
+            mutation={create}
+            closeForm={() => setVisibleForm(false)}
+          />
+        }
         style={style}
         inverted
         on="click"
@@ -30,8 +43,14 @@ function AnomaliesList({ anomalies, create, update, remove }: AnomaliesListProps
         onOpen={() => setVisibleForm(true)}
         position="top center"
       />
-      {anomalies.map(a => (
-        <AnomalyItem key={a.id} anomaly={a} update={ update } remove={ remove }/>
+      <Pagination
+        className=''
+        defaultActivePage={1}
+        totalPages={anomalies.length / 2}
+        onPageChange={(e, { activePage }) => pagination.jump(Number(activePage))}
+      />
+      {pagination.currentData().map(a => (
+        <AnomalyItem key={a.id} anomaly={a} update={update} remove={remove} />
       ))}
     </List>
   );
