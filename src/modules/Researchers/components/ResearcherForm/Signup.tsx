e9152fab2981f12e8e0dfa48be5f32c1770cc90f
luisdev97 <amadormontorodavid@gmail.com>
2020-06-03 21:30:52 +0200

    feat: Register form validated with yup

--- src/modules/Researchers/components/ResearcherForm/Signup.tsx
+++ src/modules/Researchers/components/ResearcherForm/Signup.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form } from "semantic-ui-react";
+import { Form, Label } from "semantic-ui-react";
 import ResearcherInput from "./ResearcherFormInput/ResearcherInput";
 import ResearcherButton from "./ResearcherFormInput/ResearcherButton";
 import { useForm } from "react-hook-form";
@@ -10,15 +10,30 @@ export default function Signup({ mutation }: any) {
   const watched = watch()
   const onSubmit = (data: any) => mutation(data);
 
+  function checkError(title: string) {
+    return (
+      errors.hasOwnProperty(title) && (
+        <Label basic color="red" pointing="below">
+          {errors[title].message}
+        </Label>
+      )
+    );
+  }
+
   return (
     <Form className="form" onSubmit={handleSubmit(onSubmit)}>
       <div className="control">
         <h1>Registrarse</h1>
       </div>
+      {checkError("firstname")}
       <ResearcherInput type="text" name="firstname" placeholder="Nombre" register={register}/>
+      {checkError("lastname")}
       <ResearcherInput type="text" name="lastname" placeholder="Apellidos" register={register}/>
+      {checkError("email")}
       <ResearcherInput type="text" name="email" placeholder="Email" register={register}/>
+      {checkError("password")}
       <ResearcherInput type="password" name="password" placeholder="Password" register={register}/>
+      {checkError("avatar")}
       <ResearcherInput type="file" name="avatar" placeholder="Avatar" register={register}/>
       <ResearcherButton/>
     </Form>
