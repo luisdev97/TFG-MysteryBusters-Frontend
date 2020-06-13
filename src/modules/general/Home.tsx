645e0e7b2ad4d383e4ef51501f91f31364989534
luisdev97 <amadormontorodavid@gmail.com>
2020-06-13 13:47:53 +0200

    feat: Home page created

--- src/modules/general/Home.tsx
+++ src/modules/general/Home.tsx
@@ -1,24 +1,34 @@
 import React from 'react'
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Segment, Button } from 'semantic-ui-react';
 import './Home.css'
+import { AuthContext } from '../Context/AuthContext';
 
 export default function Home() {
     return (
-        <Container text classNameName="home-container w-100">
-            <div className="home-container bg-dark">
-                <div className="home-container-top">
-                    <div className="home-container-bound home-container-bound-full"></div>
-                    <p><span className="home-container-drop home-container-home-containerranger-s">M</span><span className="home-container-home-containerranger-t">y</span><span className="home-container-home-containerranger-r">s</span><span className="home-container-home-containerranger-a">t</span><span className="home-container-home-containerranger-n">e</span><span className="home-container-home-containerranger-g">r</span><span className="home-container-home-containerranger-e">y</span>
-                    </p>
-                    <div className="home-container-bound home-container-bound-mini home-container-bound-left"></div>
-                    <div className="home-container-bound home-container-bound-mini home-container-bound-right"></div>
-                </div>
+        <AuthContext.Consumer>
+            {({ me }) => (
+                <Container className="bg-dark home-container">
+                    <div className="st bg-dark">
+                        <div className="st-top">
+                            <div className="st-bound st-bound-full"></div>
+                            <p><span className="st-drop st-stranger-s">M</span><span className="st-stranger-t">y</span><span className="st-stranger-r">s</span><span className="st-stranger-a">t</span><span className="st-stranger-n">e</span><span className="st-stranger-g">r</span><span className="st-stranger-e">y</span>
+                            </p>
+                            <div className="st-bound st-bound-mini st-bound-left"></div>
+                            <div className="st-bound st-bound-mini st-bound-right"></div>
+                        </div>
 
-                <div className="home-container-bottom">
-                    <p><span className="home-container-things-t">B</span><span className="home-container-things-h">h</span><span className="home-container-things-i">u</span><span className="home-container-things-n">s</span><span className="home-container-things-t">t</span><span className="home-container-things-x">e</span>  <span className="home-container-things-x">r</span> <span className="home-container-things-x">s</span>  </p>
-                </div>
+                        <div className="st-bottom">
+                            <p><span className="st-things-t">B</span><span className="st-things-i">u</span><span className="st-things-n">s</span><span className="st-things-t">t</span><span className="st-things-x">e</span>  <span className="st-things-x">r</span> <span className="st-things-x">s</span>  </p>
+                        </div>
 
-            </div>
-        </Container>
+                    </div>
+                    <Segment compact className="bg-dark st-text w-50 mx-auto home-container-description">
+                        <p>Bienvenidos cibernautas, en Mystery Busters hallaran un lugar donde satisfacer su extraña curiosidad, podrá explorar los sucesos más extraños que jamas haya visto, incluso participar como un investigador activo de nuestra plataforma</p>
+                        {!me && <Button fluid className="home-button" content="Unete a la comunidad" />}
+                    </Segment>
+                </Container>
+            )}
+        </AuthContext.Consumer>
+
     )
 }
